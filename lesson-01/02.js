62402c5c3c680ab7bcee6cbd5854c5b08e5122d0
Kirill <kirill0392@gmail.com>
2024-03-26 14:03:25 +0300

    random number.1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,10 +1,4 @@
 let number1 = Math.floor(Math.random() * 100);
+let result = number1 % 2 === 0 ? 'Это четное число' : 'Это нечетное число';
 
-function name(number) {
-  if (number % 2 === 0) {
-    console.log(`${number} :Это четное число`);
-  } else {
-    console.log(`${number} :Это нечетное число`);
-  }
-}
-name(number1);
+console.log(`${number1}`, result);

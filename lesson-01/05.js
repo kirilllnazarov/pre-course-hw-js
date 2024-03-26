63c9d7664b6fa679bf07c6e3514430a667d53af2
Kirill <kirill0392@gmail.com>
2024-03-26 14:44:29 +0300

    rule length.1

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,7 +1,7 @@
 function trueProgrammer () {
     let rule =
     "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
-    switch (rule) {
+    switch (true) {
       case rule.length < 25:
         console.log("Все таки нет правил без исключения");
         break;

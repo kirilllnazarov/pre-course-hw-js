69f7726cec1009c24fb85bc7e60832190bd91a41
Kirill <kirill0392@gmail.com>
2024-03-30 16:25:20 +0300

    03/07.1

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -1,7 +1,16 @@
 const numbers = [
-    [1, 2, 3, 4, 5],
-    [11, 20, 33, 40, 55],
-    [111, 200, 333, 400, 555],
+	[1, 2, 3, 4, 5],
+	[11, 20, 33, 40, 55],
+	[111, 200, 333, 400, 555],
 ];
 
+const [one, two, three] = numbers;
 
+let a = one.filter((el) => el % 2 === 0);
+let b = two.filter((el) => el % 2 === 0);
+let c = three.filter((el) => el % 2 === 0);
+
+numbers.length = 0;
+numbers.push(a, b, c);
+
+console.log(numbers);

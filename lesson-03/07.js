e5ca669f4376d98fc001a11d1b16a7536cdc3c74
Kirill <kirill0392@gmail.com>
2024-03-30 18:22:43 +0300

    03/07.2

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -4,13 +4,5 @@ const numbers = [
 	[111, 200, 333, 400, 555],
 ];
 
-const [one, two, three] = numbers;
-
-let a = one.filter((el) => el % 2 === 0);
-let b = two.filter((el) => el % 2 === 0);
-let c = three.filter((el) => el % 2 === 0);
-
-numbers.length = 0;
-numbers.push(a, b, c);
-
-console.log(numbers);
+let even = numbers.flat().filter((el) => el % 2 === 0);
+console.log(even);
